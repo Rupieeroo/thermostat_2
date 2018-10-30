cfe34209dab4500a056a3a0517cba7400394ab99
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-10-30 15:10:28 +0000

    Specification:You can decrease the temperature with a down function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,9 +7,15 @@ describe("thermostat", function() {
       expect(thermostat.temperature()).toEqual(20);
     });
 
-    it('increases the temperature', function() {
+    it('increases the temperature from 20 to 25', function() {
       thermostat.up(5);
       expect(thermostat.temperature()).toEqual(25);
     });
+
+    it('decreases the temperature from 25 to 20', function() {
+      thermostat.down(5);
+      expect(thermostat.temperature()).toEqual(20);
+    });
   });
+
 });
