90e46466e0dde106a9c20f796e29bd4f82fd58c1
Rupert Adams <RupertNCAdams@aol.com>
2018-11-01 16:36:26 +0000

    added API

--- src/interface.js
+++ src/interface.js
@@ -31,6 +31,14 @@ $(document).ready(function() {
               }
             })
 
+            $('#select-city').submit(function(event) {
+              event.preventDefault();
+              var city = $('#current-city').val();
+              $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+                $('#current-temperature').text(data.main.temp);
+              })
+            })
+
            function updateTemperature() {
              $('#temperature').text(thermostat.temperature());
              $('#temperature').attr('class', thermostat.energyUsage());
